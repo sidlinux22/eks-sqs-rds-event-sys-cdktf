aa8a6c650ff89c4381c1c9a0251e38441ca5977a
sidlinux22 <siddhartsharma@gmail.com>
2023-11-06 19:05:37 +0900

    feat(cdktf) add data stack for rds instance

--- main.ts
+++ main.ts
@@ -1,21 +1,37 @@
 import { App } from "cdktf";
-import { defaultRegion } from "./stacks/common/awsContants";
+import { AwsDataStack } from "./stacks/data/awsDataStack";
 import { AwsNetworkStack } from "./stacks/network/awsVpcStack";
+import { defaultRegion } from "./stacks/common/awsContants";
+
 
 const app = new App();
 
-// 1 VPC resources and networking 
-new AwsNetworkStack(app, "dev-network-stacks-ap", {
-  environment: "dev",
-  service: "net",
-  region: defaultRegion,
-  vpcCidrBlock: "192.168.0.0/16",
-  subnetPvtCidrA: "192.168.0.0/18",
-  subnetPvtCidrB: "192.168.64.0/18",
-  subnetPubCidrA: "192.168.128.0/18",
-  subnetPubCidrB: "192.168.192.0/18",
-});
 
+// 1. VPC resources and networking
+const vpcConfig = {
+  environment: "dev",
+  service: "net", // Service name for networking stack
+  region: defaultRegion,
+  vpcCidrBlock: "192.168.0.0/16",// CIDR block for the VPC
+  subnetPvtCidrA: "192.168.0.0/18",  // CIDR block for private subnet A
+  subnetPvtCidrB: "192.168.64.0/18", // CIDR block for private subnet B
+  subnetPubCidrA: "192.168.128.0/18", // CIDR block for public subnet A
+  subnetPubCidrB: "192.168.192.0/18", // CIDR block for public subnet B
+};
+
+// Create the AWSNetworkStack using the defined configuration
+new AwsNetworkStack(app, 'dev-network-stacks-ap', vpcConfig);
+
+// 2. Data and database
+const dataConfig = {
+  environment: "dev",
+  service: 'rds',  // Service name for RDS
+  instanceClass: 'db.t3.micro',  // RDS instance class
+  multiAz: false,  // Multi-AZ setting
+  region: defaultRegion,  // Specific AWS region
+};
+
+// Create the AWSDataStack using the defined configuration
+new AwsDataStack(app, 'dev-data-db-stacks-ap', dataConfig);
 
 app.synth();
-
---
+++ stacks/data/awsDataStack.ts
@@ -0,0 +1,81 @@
+import { Construct } from 'constructs';
+import { DataTerraformRemoteStateS3, S3Backend, TerraformStack } from 'cdktf';
+import { DbSubnetGroup } from '@cdktf/provider-aws/lib/db-subnet-group';
+import { AwsProvider } from '@cdktf/provider-aws/lib/provider';
+
+import { applicationName, devDbPassword } from '../common/awsContants';
+import { DbInstance } from '@cdktf/provider-aws/lib/db-instance';
+import { SsmParameter } from '@cdktf/provider-aws/lib/ssm-parameter';
+// import { SsmParameter } from '@cdktf/provider-aws/lib/ssm-parameter';
+
+
+export interface RdsStackConfig {
+  domainCertArn?: string;
+  environment: string;
+  service: string;
+  region: string;
+  appType?: string;
+  multiAz?: boolean;
+  instanceClass: string;
+}
+
+export class AwsDataStack extends TerraformStack {
+  constructor(scope: Construct, id: string, config: RdsStackConfig) {
+    super(scope, id);
+
+    // Create an AWS Provider
+    new AwsProvider(this, 'aws', {
+      region: config.region,
+    });
+
+    // Set up S3 Backend for state storage
+    new S3Backend(this, {
+        bucket: `${config.environment}-event-sys-tfstate`,
+        key: "awsRdsStack-cdktf",
+        region: config.region,
+      });
+  
+
+    // Define a resource name
+    const nameResource = `${config.environment}-${applicationName}-${config.service}`;
+    const rdsPassword = new SsmParameter(this, `${nameResource}-ssm-param`, {
+        name: `${nameResource}-db`,
+        type: 'String',
+        value: `${devDbPassword}`,
+      });
+
+    // Retrieve VPC information from remote state
+    const vpcRemoteData = new DataTerraformRemoteStateS3(this, "vpcRemoteData", {
+      bucket: `${config.environment}-event-sys-tfstate`,
+      key: "awsNetworkStack-cdktf",
+      region: "ap-northeast-1",
+    });
+
+    // Create a security group for RDS using subnet data from the remote state
+    const dataRemoteDbSecurity = new DbSubnetGroup(this, `${nameResource}-db-security`, {
+      name: `${nameResource}-db-security`,
+      subnetIds: vpcRemoteData.getList("privateSubnets_id"),
+    });
+
+    // Create an RDS database instance
+    new DbInstance(this, `${nameResource}-db`, {
+      identifier: `${nameResource}-db`,
+      engine: "mysql",
+      engineVersion: "8.0.31",
+      instanceClass: `${config.instanceClass}`,
+      multiAz: config.multiAz,
+      allocatedStorage: 120,
+      username: "admin",
+      password: rdsPassword.value,
+      dbSubnetGroupName: dataRemoteDbSecurity.name,
+      availabilityZone: "ap-northeast-1a",
+      deletionProtection: false,
+      maintenanceWindow: "Mon:00:00-Mon:03:00",
+      backupWindow: "03:00-06:00",
+      backupRetentionPeriod: 7,
+      skipFinalSnapshot: true,
+      allowMajorVersionUpgrade: true,
+      publiclyAccessible: false,
+    });
+  }
+}
